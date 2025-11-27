c98d0b76dce4d15e759c39dd09ba0d993eb4c755
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2025-11-27 22:33:47 +0100

    update server/CORE/server.js

--- server/CORE/server.js
+++ server/CORE/server.js
@@ -1,29 +1,14 @@
 // server/CORE/server.js
-const http = require('http');
+const https = require('https');
+const serverHandler = require('./utils/serverHandler');
 const logger = require('./utils/logger');
 
-const port = 3000;
+const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  logger.info(`Request received: ${req.method} ${req.url}`);
+const server = https.createServer(serverHandler);
 
-  if (req.method === 'GET' && req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from Node HTTP server!');
-  } else if (req.method === 'POST' && req.url === '/echo') {
-    let body = '';
-    req.on('data', chunk => body += chunk);
-    req.on('end', () => {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ received: JSON.parse(body) }));
-      logger.info(`POST /echo with body: ${body}`);
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
-  }
+server.listen(PORT, () => {
+  logger.info(`Server running on https://localhost:${PORT}`);
 });
 
-server.listen(port, () => {
-  logger.info(`Server running at http://localhost:${port}`);
-});
+module.exports = server;
