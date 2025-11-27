d4951194e10be0bc39a4c98658c894810a05dade
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2025-11-27 22:36:11 +0100

    update server/CORE/server.js

--- server/CORE/server.js
+++ server/CORE/server.js
@@ -1,11 +1,15 @@
 // server/CORE/server.js
 const https = require('https');
+
+const registerRoutes = require('./routes');
 const serverHandler = require('./utils/serverHandler');
 const logger = require('./utils/logger');
 
 const PORT = 3000;
 
 const server = https.createServer(serverHandler);
+
+registerRoutes();
 
 server.listen(PORT, () => {
   logger.info(`Server running on https://localhost:${PORT}`);
