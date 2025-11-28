889de35ae2190ca711f7762ecef245463f31ba74
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2025-11-28 11:54:26 +0100

    update server/CORE/scripts/01.run-tests.sh

--- server/CORE/scripts/01.run-tests.sh
+++ server/CORE/scripts/01.run-tests.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Wait until server is ready
+#!/bin/bash
+
+
 echo "Waiting for server to be ready..."
+echo $SERVER_FULL_URL
+
 for i in {1..10}; do
   if curl --silent --output /dev/null "$SERVER_FULL_URL"; then
     echo "Server is up!"
     break
   fi
+
   echo "Server not ready yet, retrying..."
   sleep 1
 done
 
-# Jetzt Tests ausführen
+# Tests starten
 node server/CORE/controllers/testController.js
