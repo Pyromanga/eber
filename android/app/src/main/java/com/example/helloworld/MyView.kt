d22114af51c8c8d4e8db30d9f90261a81314334d
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2025-11-30 12:55:20 +0100

    update android/app/src/main/java/com/example/helloworld/MyView.kt

--- android/app/src/main/java/com/example/helloworld/MyView.kt
+++ android/app/src/main/java/com/example/helloworld/MyView.kt
@@ -4,16 +4,25 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.util.AttributeSet
 import android.view.View
 
-class MyView(context: Context) : View(context) {
+class MyView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+    var mode: String = "none"  // "dot" oder "line"
+
     private val paint = Paint().apply {
         color = Color.RED
         strokeWidth = 10f
+        style = Paint.Style.FILL
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawLine(50f, 50f, 300f, 300f, paint)
+
+        when (mode) {
+            "dot" -> canvas.drawCircle(width/2f, height/2f, 30f, paint)
+            "line" -> canvas.drawLine(50f, 50f, width-50f, height-50f, paint)
+        }
     }
 }
