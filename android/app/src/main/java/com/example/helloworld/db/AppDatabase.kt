f9a93d956072f2b25626e5d61dfcc6f8f18a42f5
Pyromanga <155721445+Pyromanga@users.noreply.github.com>
2025-11-30 13:59:54 +0100

    update android/app/src/main/java/com/example/helloworld/db/AppDatabase.kt

--- android/app/src/main/java/com/example/helloworld/db/AppDatabase.kt
+++ android/app/src/main/java/com/example/helloworld/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import android.content.Context
 
-@Database(entities = [Point::class], version = 1)
+@Database(entities = [Point::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun pointDao(): PointDao
 
